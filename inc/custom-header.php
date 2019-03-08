9244034f41365897764992bf485078b161386423
Cosmin <cchiriac@live.com>
2019-03-08 23:29:32 +0200

    Initial commit: clone the _s theme and rename the relevant texts following the readme file rules.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -8,33 +8,33 @@
  *
  * @link https://developer.wordpress.org/themes/functionality/custom-headers/
  *
- * @package _s
+ * @package debborregcee
  */
 
 /**
  * Set up the WordPress core custom header feature.
  *
- * @uses _s_header_style()
+ * @uses debborregcee_header_style()
  */
-function _s_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( '_s_custom_header_args', array(
+function debborregcee_custom_header_setup() {
+	add_theme_support( 'custom-header', apply_filters( 'debborregcee_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
 		'width'                  => 1000,
 		'height'                 => 250,
 		'flex-height'            => true,
-		'wp-head-callback'       => '_s_header_style',
+		'wp-head-callback'       => 'debborregcee_header_style',
 	) ) );
 }
-add_action( 'after_setup_theme', '_s_custom_header_setup' );
+add_action( 'after_setup_theme', 'debborregcee_custom_header_setup' );
 
-if ( ! function_exists( '_s_header_style' ) ) :
+if ( ! function_exists( 'debborregcee_header_style' ) ) :
 	/**
 	 * Styles the header image and text displayed on the blog.
 	 *
-	 * @see _s_custom_header_setup().
+	 * @see debborregcee_custom_header_setup().
 	 */
-	function _s_header_style() {
+	function debborregcee_header_style() {
 		$header_text_color = get_header_textcolor();
 
 		/*
